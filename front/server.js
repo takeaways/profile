eb93eb875b18caa65192e3fce3b4f95db02f8b7c
nomadGeonilJang <wkdrjsdlf2@gmail.com>
2019-08-23 22:35:29 +0900

    next with express

---
+++ front/server.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const next = require('next');
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const expressSession = require('express-session');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const dev = process.env.NODE_ENV !== 'production';
+const prod = process.env.NODE_ENV === 'production';
+
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+
+app.prepare().then(() => {
+  const server = express();
+
+  server.use(morgan('dev'));
+  server.use(express.json());
+  server.use(express.urlencoded({ extended: true }));
+  server.use(cookieParser(process.env.COOKIE_SECRET));
+  server.use(expressSession({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.COOKIE_SECRET,
+    cookie: {
+      httpOnly: true,
+      secure: false,
+    },
+  }));
+
+  server.get('/hashtag/:tag', (req, res) => {
+    return app.render(req, res, '/hashtag', { tag: req.params.tag });
+  });
+
+  server.get('/user/:id', (req, res) => {
+    return app.render(req, res, '/user', { id: req.params.id });
+  });
+
+
+  server.get('*', (req, res) => {
+    //get 요청 처리기
+    return handle(req, res);
+  });
+
+  server.listen(process.env.PORT, () => {
+    console.log(`next+express running on port ${process.env.PORT}`);
+  });
+});
